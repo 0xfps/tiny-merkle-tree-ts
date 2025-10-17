b5237b58d0b118e8302d5d6dc99d1c90fee72c7a
0xfps <anthony.nnaemeka.umeh@gmail.com>
2025-10-17 07:53:15 +0100

    refactor: make function clearer

--- src/utils/get-leaf-from-key.ts
+++ src/utils/get-leaf-from-key.ts
@@ -2,13 +2,13 @@ import { poseidon } from "poseidon-hash";
 import { extractKeyMetadata } from "../contract-utils/extract-key-metadata";
 import { smolPadding } from "./smol-padding";
 
-export function getLeafFromKey(withdrawalKey: string): string {
-    const { keyHash, amountU32 } = extractKeyMetadata(withdrawalKey)
+export function getLeafFromKey(depositKey: string): string {
+    const { keyHash, amountU32 } = extractKeyMetadata(depositKey)
 
-    const wKeyBigInt = BigInt(keyHash)
+    const dKeyBigInt = BigInt(keyHash)
     const amountBigInt = BigInt(amountU32)
 
-    const leafNum = poseidon([wKeyBigInt, amountBigInt])
+    const leafNum = poseidon([dKeyBigInt, amountBigInt])
     const leaf = smolPadding(`0x${leafNum.toString(16)}`)
     return leaf
 }
